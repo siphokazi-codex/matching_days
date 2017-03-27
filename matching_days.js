7e577c8093f4e23a2562692d20eca79f5236b43c
siphokazi-codex <aplenis@webmail.co.za>
2017-03-27 15:55:44 +0200

    My second commit

--- matching_days.js
+++ matching_days.js
@@ -3,6 +3,7 @@ var match = document.querySelector('.btnMatch');
 var firstDay = document.querySelector('.txtFirst');
 var secondDay = document.querySelector('.txtSecond');
 var list = document.querySelectorAll('td');
+var diff = document.querySelector('.different');
 
 function sameWeekday () {
 var dateOne = new Date(firstDay.value);
@@ -11,20 +12,22 @@ var dateTwo = new Date(secondDay.value);
 var dayOne = dateOne.getDay();
 var dayTwo = dateTwo.getDay();
 
+var ul = document.getElementsByTagName('ul')[0];
+//var li = document.createElement('li');
+
 //clear the background color on the fields
 refreshDays ();
 
 if (dayOne === dayTwo)
 {
   list[dayOne].className = 'greenDay';
-  //console.console.log(list[dayOne]);
-  //list[dayOne].classList.add('greenDay');
-  //console.log("date is " + dayOne);
+  ul.innerHTML = "The two selected dates, falls on the same week day, hence the week day is in green color";
 }
 else if (dayOne !== dayTwo)
 {
   list[dayOne].className = 'blueDay';
   list[dayTwo].className = 'redDay';
+  ul.innerHTML = "The two selected dates, falls on different days, hence the days for each date is in a different color";
 }
 }
 
